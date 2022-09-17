304710c5e60f4274acc04dbe40fbfa2af094efc9
risavh <risavh12@gmail.com>
2022-09-17 22:47:55 +1000

    countries output

--- D3JS/app.js
+++ D3JS/app.js
@@ -1,6 +1,16 @@
+const DUMMY_DATA = [
+{'id':'d1', value:10,'region': 'USA'},
+{'id':'d2', value:20,'region': 'Australia'},
+{'id':'d3', value:5,'region': 'India'},
+{'id':'d4', value:42,'region': 'China'}
+
+]
+
+
+
 d3.select('div')
     .selectAll('p')
-    .data([1,2,3])
+    .data(DUMMY_DATA)
     .enter()
     .append('p')
-    .text(dta => dta);
+    .text(dta => dta.region);
